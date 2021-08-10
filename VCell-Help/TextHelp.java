0105b20b373c9ceb3a229520bc89124b49cabf3e
NIKITA MAHOVIYA <43717626+nikitamahoviya@users.noreply.github.com>
2021-08-11 00:05:42 +0530

    Update TextHelp.java
    Changed the text alignment from right to left

--- VCell-Help/TextHelp.java
+++ VCell-Help/TextHelp.java
@@ -31,7 +31,6 @@
 
 package org.vcell.imagej.plugin;
 
-
 import java.awt.Dimension;
 import org.scijava.ItemIO;
 import org.scijava.ItemVisibility;
@@ -65,12 +64,6 @@ import javax.swing.JLabel;
 import javax.swing.JProgressBar;
 import javax.swing.JToolBar;
 
-/**
- * A test of text displays.
- * 
- * @author Curtis Rueden
- */
-
 @Plugin(type = Command.class, label = "VCell-ImageJ Help",  iconPath="help.gif",menu = {
 		@Menu(label = MenuConstants.PLUGINS_LABEL, weight = MenuConstants.FILE_WEIGHT,
 			mnemonic = MenuConstants.FILE_MNEMONIC),
@@ -78,92 +71,91 @@ import javax.swing.JToolBar;
 		@Menu(label = "VCell-ImageJ Help", weight = 9,
 			mnemonic = 'h',accelerator = "^H") },  attrs = { @Attr(name = "no-legacy") })
 public class TextHelp implements Command, Previewable {
-//"(c) Copyright 1998-2020 UConn Health"
 	
 	@Parameter
 	private UIService uiService;
 
-	@Parameter(visibility = ItemVisibility.MESSAGE)
-	private final String header1 = "<html><h2 style=\"font-family:'Times New Roman';color:green;\">Hello " + System.getProperty("user.name") + " !  Welcome to VCell Help</h2><html>";
+	@Parameter( label = " " , visibility = ItemVisibility.MESSAGE)
+	private final String header1 = "<html><h2 style=\"font-family:'Times New Roman';color:green;font-size:25px\">Hello " + System.getProperty("user.name") + " !  Welcome to VCell Help</h2><html>";
 
-	@Parameter(visibility = ItemVisibility.MESSAGE)
+	@Parameter(label = " " ,visibility = ItemVisibility.MESSAGE)
 	private final String header2 = "<html><h3 style=\"font-family:'Optima';color:red;\">About your machine </h2><html>";
 
-	@Parameter(visibility = ItemVisibility.MESSAGE) 
+	@Parameter(label = " " ,visibility = ItemVisibility.MESSAGE) 
 	private final String header3 = "<html><pstyle=\"font-family:'Optima'\">Java Version: "+" " + System.getProperty("java.version") +"<br> Operating System: "+" " +System.getProperty("os.name")+ " " + System.getProperty("os.arch")+"<br>(c) Copyright 1998-2020 UConn Health</p><html>";
 	
-	@Parameter(visibility = ItemVisibility.MESSAGE)
+	@Parameter(label = " " ,visibility = ItemVisibility.MESSAGE)
 	private final String header4 = "<html><h3 style=\"font-family:'Optima';color:red;\">Pre-Requirements for running VCell Plugins </h2><html>";
 
-	@Parameter(visibility = ItemVisibility.MESSAGE) 
+	@Parameter(label = " " ,visibility = ItemVisibility.MESSAGE) 
 	  private final String header5 = "<html><p style=\"font-family:'Optima'\">Virtual Cell is an advanced software platform for modeling and simulating reaction <br> "
 		  		+ " kinetics,membrane transport and diffusion in the complex geometries</p><html>";
 	
-	@Parameter(visibility = ItemVisibility.MESSAGE) 
+	@Parameter(label = " " ,visibility = ItemVisibility.MESSAGE) 
 	private final String header6 = "<html><p style=\"font-family:'Optima'\">To run various VCell Plugins there are certain pre requirements which are listed below <p> "
 		  		+ "<ul> <li>VCell Client ImageJ service should be activated.</li>"
 		  		+"<li>Check your connectivity and login to VCell with your userId or as a guest.</li></ul></html>";
 	
-	@Parameter(visibility = ItemVisibility.MESSAGE)
+	@Parameter(label = " " ,visibility = ItemVisibility.MESSAGE)
 	private final String header7 = "<html><h3 style=\"font-family:'Optima';color:red;\">Online Resources</h2><html>";
 	
-	@Parameter(visibility = ItemVisibility.MESSAGE) 
+	@Parameter(label = " " ,visibility = ItemVisibility.MESSAGE) 
 	private final String header08 = "<html><p style=\"font-family:'Optima'\">To know more about VCell, visit our website. </p><html>";
 
 	@Parameter(label="VCell Webiste", callback = "website")
 	private Button buttonWebsite;
 	
-	@Parameter(visibility = ItemVisibility.MESSAGE) 
+	@Parameter(label = " " , visibility = ItemVisibility.MESSAGE) 
 	private final String header09 = "<html><p style=\"font-family:'Optima'\">Having doubts or want to discuss with the community, visit the discussion forum</p><html>";
 
 	@Parameter(label="Disucssion Forum", callback = "forum")
 	private Button buttonForum;
 	
-	@Parameter(visibility = ItemVisibility.MESSAGE) 
+	@Parameter(label = " " ,visibility = ItemVisibility.MESSAGE) 
 	private final String header010 = "<html><p style=\"font-family:'Optima'\">Want to learn about changing permission for your model, checkout the permission changing page </p><html>";
 
 	@Parameter(label="Change Permission", callback = "permission")
 	private Button buttonPermission;
 	
-	@Parameter(visibility = ItemVisibility.MESSAGE) 
+	@Parameter(label = " " ,visibility = ItemVisibility.MESSAGE) 
 	private final String header011 = "<html><p style=\"font-family:'Optima'\">Want to make your model public, checkout the publishing model guidelines</p><html>";
 
 	@Parameter(label="Publish your Model", callback = "publish")
 	private Button buttonModel;
 	
-	@Parameter(visibility = ItemVisibility.MESSAGE) 
+	@Parameter(label = " " ,visibility = ItemVisibility.MESSAGE) 
 	private final String header012 = "<html><p style=\"font-family:'Optima'\">Visit VCell Support for your encountered problems</p><html>";
 
 	@Parameter(label="Contact Support", callback = "support")
 	private Button buttonSupport;
 
-	@Parameter(visibility = ItemVisibility.MESSAGE) 
+	@Parameter(label = " " ,visibility = ItemVisibility.MESSAGE) 
 	  private final String header8 = "<html><p style=\"font-family:'Optima'\">Featuring COPASI prameter estimation technology </p><html>";
 	
 	@Parameter(label="Visit COPASI", callback = "copasi")
 	private Button buttonCopasi;
 	
-	@Parameter(visibility = ItemVisibility.MESSAGE) 
+	@Parameter(label = " " ,visibility = ItemVisibility.MESSAGE) 
 	  private final String header9 = "<html><p style=\"font-family:'Optima'\">Featuring spatial stochastic simulations powered by SMOLDYN </p><html>";
 	
 	@Parameter(label="Visit SMOLYDYN", callback = "smoldyn")
 	private Button buttonSmoldyn;
 	
-	@Parameter(visibility = ItemVisibility.MESSAGE) 
+	@Parameter(label = " " ,visibility = ItemVisibility.MESSAGE) 
 	  private final String header10 = "<html><p style=\"font-family:'Optima'\">Featuring rule-based simulations powered by BIONETGEN </p><html>";
 	
 	@Parameter(label="Visit BIONETGEN", callback = "bionetgen", description = "help.gif")
 	private Button buttonBionetgen;
 	
-	@Parameter(visibility = ItemVisibility.MESSAGE) 
+	@Parameter(label = " " ,visibility = ItemVisibility.MESSAGE) 
 	  private final String header11 = "<html><p style=\"font-family:'Optima'\">Featuring network-free stochastic simulations powered by NFsim </p><html>";
 	
 	@Parameter(label="Visit NFsim", callback = "nfsim")
 	private Button buttonNfsim;
 	
-	@Parameter(visibility = ItemVisibility.MESSAGE) 
-	  private final String header12 = "<html><p style=\"font-family:'Optima';color: blue;\">Virtual Cell is supported by NIH Grant R24 GM137787 from the National Institute <br> "
-		  		+ " for General Medical Sciences</p><html>";
+	@Parameter(label = " " ,visibility = ItemVisibility.MESSAGE) 
+	  private final String header12 = "<html><p style=\"font-family:'Optima';color: blue;\">Virtual Cell is supported by NIH Grant R24 GM137787 from the National Institute for <br> "
+		  		+ " General Medical Sciences</p><html>";
 	
 	public static void main(final String... args) throws Exception {
         // create the ImageJ application context with all available services
